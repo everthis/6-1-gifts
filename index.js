83b648d610698d06c526d2d6bc4b1e2e642ad9ef
hejiewwwaap <hejiewwwaap@didichuxing.com>
2018-05-22 17:58:26 +0800

    add micromessager animation

--- index.js
+++ index.js
@@ -13,7 +13,27 @@ function tpl(el, idx) {
   const title = `${idx + 1}. ${el.title}`
   return `<div class='gift'><h3>${title}</h3>${imgHtmlStr(el.images)}</div>`
 }
+function doScrolling(elementY, duration) {
+  const startingY = window.pageYOffset
+  const diff = elementY - startingY
+  let start
 
+  // Bootstrap our animation - it will get called right before next frame shall be rendered.
+  window.requestAnimationFrame(function step(timestamp) {
+    if (!start) start = timestamp
+    // Elapsed milliseconds since start of scrolling.
+    const time = timestamp - start
+    // Get percent of completion in range [0, 1].
+    const percent = Math.min(time / duration, 1)
+
+    window.scrollTo(0, startingY + diff * percent)
+
+    // Proceed with animation as long as we wanted it to.
+    if (time < duration) {
+      window.requestAnimationFrame(step)
+    }
+  })
+}
 const appHtmlStr = imgs.map(tpl).join('')
 
 const appEle = document.getElementById('app')
@@ -22,5 +42,5 @@ const backToTopEle = document.getElementById('backToTop')
 appEle.innerHTML = appHtmlStr
 
 backToTopEle.addEventListener('click', function(ev) {
-  appTitleEle.scrollIntoView({ block: 'end', behavior: 'smooth' })
+  doScrolling(0, 200)
 })
